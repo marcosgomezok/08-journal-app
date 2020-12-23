70fba799b8c00606648a7d764ba6ded4b1b45c6c
Marcos Gomez <marcos_go2015@hotmail.com>
2020-12-23 15:33:19 -0300

    10. Variables de entorno

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,31 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB3Ej5wR4wSVQ2HoL76uFIDoe_MecC-29Q",
-    authDomain: "react-app-cursos-mg.firebaseapp.com",
-    databaseURL: "https://react-app-cursos-mg.firebaseio.com",
-    projectId: "react-app-cursos-mg",
-    storageBucket: "react-app-cursos-mg.appspot.com",
-    messagingSenderId: "516377201660",
-    appId: "1:516377201660:web:6a781199b345fe5f384079"
-  };
-const firebaseConfigTesting = {
-    apiKey: "AIzaSyBtH-PyDLFGRkmWFFhF2QEx23l0PgrWoQ4",
-    authDomain: "redux-demo-2d6c2.firebaseapp.com",
-    projectId: "redux-demo-2d6c2",
-    storageBucket: "redux-demo-2d6c2.appspot.com",
-    messagingSenderId: "717103744620",
-    appId: "1:717103744620:web:efb61bce9ee858a1aaba49",
-    measurementId: "G-C2NT1HXPET"
-  };
-if(process.env.NODE_ENV ==='test'){
-  //testing
-  firebase.initializeApp(firebaseConfigTesting);
-}else{
-  //dev/prod
-  firebase.initializeApp(firebaseConfig);
-}
+//console.log(process.env)
+
+ const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+}; 
+firebase.initializeApp(firebaseConfig)
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
@@ -37,3 +24,35 @@ export {
     googleAuthProvider,
     firebase
 }
+
+
+
+
+/*
+    const firebaseConfig = {
+        apiKey: "AIzaSyB3Ej5wR4wSVQ2HoL76uFIDoe_MecC-29Q",
+        authDomain: "react-app-cursos-mg.firebaseapp.com",
+        databaseURL: "https://react-app-cursos-mg.firebaseio.com",
+        projectId: "react-app-cursos-mg",
+        storageBucket: "react-app-cursos-mg.appspot.com",
+        messagingSenderId: "516377201660",
+        appId: "1:516377201660:web:6a781199b345fe5f384079"
+      };
+    const firebaseConfigTesting = {
+      apiKey: "AIzaSyBtH-PyDLFGRkmWFFhF2QEx23l0PgrWoQ4",
+      authDomain: "redux-demo-2d6c2.firebaseapp.com",
+      databaseURL: "https://redux-demo-2d6c2-default-rtdb.firebaseio.com",
+      projectId: "redux-demo-2d6c2",
+      storageBucket: "redux-demo-2d6c2.appspot.com",
+      messagingSenderId: "717103744620",
+      appId: "1:717103744620:web:fb602a21c550d904aaba49",
+    
+      };
+    if(process.env.NODE_ENV ==='test'){
+      //testing
+      firebase.initializeApp(firebaseConfigTesting);
+    }else{
+      //dev/prod
+      firebase.initializeApp(firebaseConfig);
+    }
+    */
