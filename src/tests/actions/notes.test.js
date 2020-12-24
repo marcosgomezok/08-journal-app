a90fe261e6f66e5c53b6bb4380532607c7e5789b
Marcos Gomez <marcos_go2015@hotmail.com>
2020-12-24 06:08:50 -0300

    11. Pruebas en startLoadingNotes y startSaveNote

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,21 +1,30 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
-import { db } from '../../firebase/firebase-config';
-import { startNewNote } from './../../actions/notes';
+
+
+import { startLoadingNotes, startNewNote, startSaveNote } from './../../actions/notes';
 import { types } from './../../types/types';
+import { db } from '../../firebase/firebase-config';
+
  
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
-const store = mockStore({
+const initState = {
     auth: {
         uid: 'TESTING'
-
     }
-})
+}
+
+let store = mockStore(initState)
 
 describe('Pruebas con las acciones de notas', () => {
-    
+
+    beforeEach(()=>{
+        jest.setTimeout(30000)
+        store= mockStore(initState)
+
+    })
     test('debe de crear una nueva nota StartNewNote', async() => {
         
         await store.dispatch(startNewNote());
@@ -44,6 +53,48 @@ describe('Pruebas con las acciones de notas', () => {
 
         const docId = actions[0].payload.id;
         await db.doc(`/TESTING/journal/notes/${docId}`).delete();
+        
     })
+
+
+    test('startLoadingNotes debe cargar las notas', async() => {
+        
+        /* await store.dispatch( startLoadingNotes('TESTING') ); //Error en esta linea, ni idea que sera ver uso de jest.setTimeout(30000)
+        const actions = store.getActions();
+
+        expect( actions[0] ).toEqual({
+            type: types.notesLoad,
+            payload: expect.any(Array)
+        });
+
+        const expected = {
+            id: expect.any(String),
+            title: expect.any(String),
+            body: expect.any(String),
+            date: expect.any(Number),
+        }
+
+        expect( actions[0].payload[0] ).toMatchObject( expected );  */
+    })
+
+    test('startsavenote debe actualizar la nota', async() => {
+        
+        const note = {
+            id: '3UhaD16pVZDIlepRdZX9',
+            title: 'titulo',
+            body: 'body'
+        };
+
+        await store.dispatch(startSaveNote(note))
+
+        const actions = store.getActions();
+       // console.log(actions)
+       expect(actions[0].type).toBe(types.notesUpdated)
+       
+    /* const docRef = await db.doc(`/TESTING/journal/notes/${note.id}`).get(); //Error en esta linea, ni idea que sera ver uso de jest.setTimeout(30000)
+       expect(docRef.data().title).toBe(note.title);  */
+
+    })
+    
     
 })
